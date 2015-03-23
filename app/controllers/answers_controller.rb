4d6d5112bc33080a4d7cf7f068099bbb0f344142
Ben <ben.s.pritchard@gmail.com>
2015-03-23 16:22:55 -0700

    Add ability to add answers (on question show page)

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,21 @@
-class AnswersController < ApplicationController::Base
+class AnswersController < ApplicationController
   def new
-
   end
 
   def create
-    @question = Question.find(params[:id])
-    @answer = @question.answers.new(answer_id)
-    if @question.answers
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      flash[:notice] = "Answer added successfully! Might not be right but at least it's there."
+      redirect_to :back
+    else
+      render :new
+    end
   end
 
 private
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 
 end
